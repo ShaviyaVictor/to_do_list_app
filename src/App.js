96fa62de4efb336d6b2aa56bf3fc4faf1ae7337e
shaviyavictor <shaviyavictor@gmail.com>
2023-01-28 14:41:40 -0500

    Update the DOM with the root component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+const App = () => {
+  const [todos, setTodos] = React.useState([]);
+  const [todo, setTodo] = React.useState("");
+  
+  // Add the handlesubmit code here
+  
+  
+  // Add the deleteToDo code here
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  
+  // Add the toggleComplete code here
 
+  
+  // Add the submitEdits code here
+
+  
+return(
+<div className ="App">
+<h1>Todo List</h1>
+<form>
+<input type ="text" align ="right" />
+<button type ="submit">Add Todo</button>
+</form>
+</div>
+);
+};
 export default App;
